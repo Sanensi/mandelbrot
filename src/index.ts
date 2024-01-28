a3f55b4c2a2448723db051c9f0488fcebc397a65
Sanensi <william.picard@hotmail.com>
2024-01-28 09:44:11 -0500

    feat: Can draw a rectangle

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,11 @@
-console.log("Hello World!");
+const canvas = document.querySelector("canvas") ?? throwError();
+const ctx = canvas.getContext("2d") ?? throwError();
+
+canvas.width = canvas.clientWidth;
+canvas.height = canvas.clientHeight;
+
+ctx.strokeRect(100, 100, 500, 400);
+
+function throwError(msg?: string): never {
+  throw new Error(msg);
+}
