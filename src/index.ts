b98312a833107ede7a44bdfa3f50de3efa2167e9
Sanensi <william.picard@hotmail.com>
2024-01-28 11:19:54 -0500

    feat: Render on each frame

--- src/index.ts
+++ src/index.ts
@@ -13,19 +13,30 @@ const scale = Vec2.ONE;
 
 const image = new Image(ctx);
 
-for (let y = 0; y < canvas.height; y++) {
-  for (let x = 0; x < canvas.width; x++) {
-    const p = new Vec2(x, y);
-    const p_prime = p.substract(offset).divide(scale);
+function render(ms: number) {
+  for (let y = 0; y < canvas.height; y++) {
+    for (let x = 0; x < canvas.width; x++) {
+      const p = new Vec2(x, y);
+      const p_prime = p.substract(offset).divide(scale);
 
-    if (isInMandelbrot(p_prime)) {
-      image.setPixel(p, 0, 0, 0);
+      if (isInMandelbrot(p_prime)) {
+        image.setPixel(p, 0, 0, 0);
+      }
     }
   }
-}
 
-ctx.putImageData(image.imageData, 0, 0);
+  ctx.putImageData(image.imageData, 0, 0);
+}
 
 function isInMandelbrot(p: Vec2) {
   return -5 <= p.x && p.x <= 5 && -5 <= p.y && p.y <= 5;
 }
+
+function renderLoop(ms: number) {
+  canvas.width = canvas.clientWidth;
+  canvas.height = canvas.clientHeight;
+  render(ms);
+  requestAnimationFrame(renderLoop);
+}
+
+requestAnimationFrame(renderLoop);
