67e27177901c5e89b901b07530dbec7178c603e0
Sanensi <william.picard@hotmail.com>
2024-01-29 20:57:16 -0500

    feat: Add zoom

--- src/webgl-based/index.ts
+++ src/webgl-based/index.ts
@@ -5,27 +5,28 @@ import { createProgram } from "./program";
 import { setScreenGeometry, drawScreen } from "./screen";
 import { Vec2 } from "../Vec2";
 
-main();
-
 type MouseState = {
   previousPosition?: Vec2;
 };
+
+const canvas = document.querySelector("canvas") ?? throwError();
+canvas.width = canvas.clientWidth;
+canvas.height = canvas.clientHeight;
+
+const gl = canvas.getContext("webgl") ?? throwError();
 
 const mouseState: MouseState = {
   previousPosition: undefined,
 };
 
+const gameState = {
+  offset: new Vec2(canvas.width / 2, canvas.height / 2),
+  scale: Vec2.ONE.scale(250),
+};
+
+main();
+
 async function main() {
-  const canvas = document.querySelector("canvas") ?? throwError();
-  canvas.width = canvas.clientWidth;
-  canvas.height = canvas.clientHeight;
-
-  const gameState = {
-    offset: new Vec2(canvas.width / 2, canvas.height / 2),
-    scale: Vec2.ONE.scale(250),
-  };
-
-  const gl = canvas.getContext("webgl") ?? throwError();
   const program = createProgram(gl, vertexShaderSource, fragmentShaderSource);
   gl.useProgram(program);
 
@@ -68,6 +69,15 @@ async function main() {
   });
   canvas.addEventListener("mouseup", () => {
     mouseState.previousPosition = undefined;
+  });
+
+  canvas.addEventListener("wheel", (e) => {
+    if (e.deltaY < 0) {
+      gameState.scale = gameState.scale.scale(1.05);
+    } else {
+      gameState.scale = gameState.scale.scale(0.95);
+    }
+    gl.uniform2fv(scaleLocation, [gameState.scale.x, gameState.scale.y]);
   });
 }
 
