a0f5c94bb0497291c608d286d73d2feac679f38d
Sanensi <william.picard@hotmail.com>
2024-02-03 18:53:44 -0500

    refactor: Merge screen function with MandelbrotRenderer

--- src/webgl-based/MandelbrotRenderer.ts
+++ src/webgl-based/MandelbrotRenderer.ts
@@ -1,17 +1,17 @@
 import { Vec2 } from "../Vec2";
 import { createProgram } from "./program";
-import { setScreenGeometry, drawScreen } from "./screen";
 import fragmentShaderSource from "./shaders/mandelbrot.glsl?raw";
 import vertexShaderSource from "./shaders/screen.glsl?raw";
 
 const POSITION_ATTRIBUTE_SIZE = 2;
-const screenVertices = [-1.0, -1.0, 1.0, -1.0, -1.0, 1.0, 1.0, 1.0];
+const SCREEN_VERTICES = [-1.0, -1.0, 1.0, -1.0, -1.0, 1.0, 1.0, 1.0];
 
 export class MandelbrotRenderer {
   private readonly gl: WebGLRenderingContext;
 
   private readonly offsetLocation: WebGLUniformLocation | null;
   private readonly scaleLocation: WebGLUniformLocation | null;
+  private readonly positionAttributeLocation: number;
 
   constructor(gl: WebGLRenderingContext) {
     this.gl = gl;
@@ -23,23 +23,41 @@ export class MandelbrotRenderer {
     this.scaleLocation = gl.getUniformLocation(program, "scale");
 
     const positionAttributeLocation = gl.getAttribLocation(program, "position");
+    this.positionAttributeLocation = positionAttributeLocation;
     gl.enableVertexAttribArray(positionAttributeLocation);
 
-    const POSITION_ATTRIBUTE_SIZE = 2;
-    const screenVertices = [-1.0, -1.0, 1.0, -1.0, -1.0, 1.0, 1.0, 1.0];
+    this.setScreenGeometry();
+  }
 
-    setScreenGeometry(
-      gl,
-      screenVertices,
-      positionAttributeLocation,
+  private setScreenGeometry() {
+    const positionBuffer = this.gl.createBuffer();
+    this.gl.bindBuffer(this.gl.ARRAY_BUFFER, positionBuffer);
+    this.gl.bufferData(
+      this.gl.ARRAY_BUFFER,
+      new Float32Array(SCREEN_VERTICES),
+      this.gl.STATIC_DRAW,
+    );
+    this.gl.vertexAttribPointer(
+      this.positionAttributeLocation,
       POSITION_ATTRIBUTE_SIZE,
+      this.gl.FLOAT,
+      false,
+      0,
+      0,
     );
   }
 
   render(offset: Vec2, scale: Vec2) {
     this.gl.uniform2fv(this.offsetLocation, [offset.x, offset.y]);
     this.gl.uniform2fv(this.scaleLocation, [scale.x, scale.y]);
+    this.drawScreen();
+  }
 
-    drawScreen(this.gl, screenVertices, POSITION_ATTRIBUTE_SIZE);
+  private drawScreen() {
+    this.gl.drawArrays(
+      this.gl.TRIANGLE_STRIP,
+      0,
+      SCREEN_VERTICES.length / POSITION_ATTRIBUTE_SIZE,
+    );
   }
 }
