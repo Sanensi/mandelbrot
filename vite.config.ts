38e6199478047ce658749a0e9854ef9caf34b948
Sanensi <william.picard@hotmail.com>
2024-02-05 21:11:58 -0500

    chore: Setup build for rust

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { UserConfig, defineConfig } from "vite";
 import { resolve } from "path";
 import wasm from "vite-plugin-wasm";
+import topLevelAwait from "vite-plugin-top-level-await";
 
 const config: UserConfig = defineConfig({
   base: "mandelbrot",
@@ -9,12 +10,14 @@ const config: UserConfig = defineConfig({
       input: {
         main: resolve(__dirname, "index.html"),
         canvas: resolve(__dirname, "pages", "canvas-based.html"),
+        rust: resolve(__dirname, "pages", "rust-based.html"),
         webgl: resolve(__dirname, "pages", "webgl-based.html"),
       },
     },
   },
   plugins: [
     wasm(),
+    topLevelAwait(),
     {
       name: "prepend-base-url-to-anchor",
       transformIndexHtml: (html) => {
